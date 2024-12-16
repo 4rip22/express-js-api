97e945a61868b77f06eae8fdc82001e2581d3922
4rip22 <kkruipputra816@gmail.com>
2024-12-16 18:46:39 +0700

    fix:port 3000 update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
-const port = process.env.PORT || 8080; // Gunakan port dari variabel lingkungan atau default ke 8080
+const port = process.env.PORT || 3000; // Gunakan port dari variabel lingkungan atau default ke 8080
 
 let model = null;
 
