55dd646ff03c8251ed265f7c57ab792cb9eb8e18
4rip22 <kkruipputra816@gmail.com>
2024-12-28 19:49:01 +0700

    fix api express

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
-const port = process.env.PORT || 3000; // Gunakan port dari variabel lingkungan atau default ke 3000
+const port = process.env.PORT || 3000; 
 
 let model = null;
 const loadModel = async () => {
